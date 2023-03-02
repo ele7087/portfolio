400d5f367f1ade76a66e40bf247718b73d60797d
andyhuang777 <andy@mercury.com>
2023-03-01 21:20:07 -0500

    Finished landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import CaseStudies from "./pages/CaseStudies";
+import Gallery from "./pages/Gallery";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar className="yay" />
+      <ParallaxProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/casestudies" element={<CaseStudies />} />
+          <Route path="/gallery" element={<Gallery />} />
+        </Routes>
+      </ParallaxProvider>
     </div>
   );
 }
